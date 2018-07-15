472e4b481751482965f18e337630064c6511a7d9
Rafael Ruiz Navas <rafa@ruiznavas.com>
2018-07-15 23:17:05 +0200

    Deshabilitado Ctrl-S y Ctrl-Q

--- redit.c
+++ redit.c
@@ -15,7 +15,8 @@ void enableRawMode(){
 	atexit(disableRawMode);
 
 	struct termios raw = orig_termios;
-
+	
+	raw.c_iflag &= ~(IXON);
 	raw.c_lflag &= ~(ECHO | ICANON | ISIG);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
