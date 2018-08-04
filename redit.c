b59081b51acd01dec8fe19e0882cedae20a6ce88
Rafael Ruiz Navas <rafa@ruiznavas.com>
2018-08-04 21:37:26 +0200

    Apagando diferentes flags

--- redit.c
+++ redit.c
@@ -16,8 +16,9 @@ void enableRawMode(){
 
 	struct termios raw = orig_termios;
 	
-	raw.c_iflag &= ~(ICRNL | IXON);
+	raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
 	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag |= (CS8);
 	raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
 
 	tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
@@ -29,9 +30,9 @@ int main(){
 	char c;
 	while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q'){
 		if(iscntrl(c)){
-			printf("%d\n",c);
+			printf("%d\r\n",c);
 		}else{
-			printf("%d ('%c')\n", c, c);
+			printf("%d ('%c')\r\n", c, c);
 		}
 	}
 
