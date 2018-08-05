1521503ad1ee7b748ed3174164a6409cd1183515
Rafael Ruiz Navas <rafa@ruiznavas.com>
2018-08-05 23:05:11 +0200

    Limpieza de pantalla en la salida

--- redit.c
+++ redit.c
@@ -14,6 +14,9 @@ struct termios orig_termios;
 
 /*** Terminal ***/
 void die(const char *s){
+	write(STDOUT_FILENO, "\x1b[2J", 4);
+	write(STDOUT_FILENO, "\x1b[H", 3);
+
 	perror(s);
 	exit(1);
 }
@@ -62,6 +65,9 @@ void editorProcessKeypress(){
 
 	switch(c){
 		case CTRL_KEY('q'):
+			write(STDOUT_FILENO, "\x1b[2J", 4);
+			write(STDOUT_FILENO, "\x1b[H", 3);
+			
 			exit(0);
 			break;
 	}
