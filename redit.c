46ded1701aa0a22ccb8a1b844b88ac659460740f
Rafael Ruiz Navas <rafa@ruiznavas.com>
2018-08-06 20:21:29 +0200

    Agregando mensaje de bienvenida y centrandolo

--- redit.c
+++ redit.c
@@ -9,6 +9,7 @@
 #include <string.h>
 
 /*** Definiciones ***/
+#define REDIT_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** Datos ***/
@@ -116,7 +117,20 @@ void abFree(struct abuf *ab){
 void editorDrawRows(struct abuf *ab){
 	int y;
 	for(y=0;y<E.screenrows;y++){
-		abAppend(ab,"~",1);
+		if(y==E.screenrows/3){
+			char welcome[80];
+			int welcomelen = snprintf(welcome, sizeof(welcome), "REDIT - Tu editor, v%s", REDIT_VERSION);
+			if(welcomelen > E.screencols) welcomelen = E.screencols;
+			int padding = (E.screencols - welcomelen) / 2;
+			if(padding){
+				abAppend(ab, "~",1);
+				padding--;
+			}
+			while(padding--) abAppend(ab, " ", 1);
+			abAppend(ab, welcome,welcomelen);
+		}else{
+			abAppend(ab,"~",1);
+		}
 
 		abAppend(ab,"\x1b[K",3);
 		if(y<E.screenrows - 1){
