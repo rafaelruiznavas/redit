681298326c283190431d868a199187ad6908fb5c
Rafael Ruiz Navas <rafa@ruiznavas.com>
2018-08-07 21:26:15 +0200

    Agrego un offset de filas para controlar el scroll vertical

--- redit.c
+++ redit.c
@@ -36,6 +36,7 @@ typedef struct erow{
 
 struct editorConfig{
 	int cx, cy;
+	int rowoff;
 	int screenrows;
 	int screencols;
 	int numrows;
@@ -316,6 +317,7 @@ void editorProcessKeypress(){
 void initEditor(){
 	E.cx = 0;
 	E.cy = 0;
+	E.rowoff = 0;
 	E.numrows = 0;
 	E.row = NULL;
 	if(getWindowSize(&E.screenrows, &E.screencols) == -1)
