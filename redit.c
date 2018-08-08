7475e725724a6aa2491005e2fa68839e5e90d9ca
Rafael Ruiz Navas <rafa@ruiznavas.com>
2018-08-08 20:33:56 +0200

    Ajustar cursor al final del fichero

--- redit.c
+++ redit.c
@@ -301,6 +301,10 @@ void editorMoveCursor(int key){
 				E.cy++;
 			break;
 	}
+	row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+	int rowlen = row ? row->size : 0;
+	if(E.cx > rowlen)
+		E.cx = rowlen;
 }
 
 void editorProcessKeypress(){
