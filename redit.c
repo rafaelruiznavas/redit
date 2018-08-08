c226b5f05aee1228fafb2c7eb2fcbd98fd93f56b
Rafael Ruiz Navas <rafa@ruiznavas.com>
2018-08-08 23:32:47 +0200

    Creo la funcion editorUpdateRow, y la usamos desde la funcion editorAppendRow

--- redit.c
+++ redit.c
@@ -163,6 +163,19 @@ int getWindowSize(int *rows, int *cols){
 }
 
 /*** Operaciones de filas ***/
+void editorUpdateRow(erow *row){
+	free(row->render);
+	row->render = malloc(row->size+1);
+
+	int j;
+	int idx = 0;
+	for(j=0;j<row->size;j++){
+		row->render[idx++] = row->chars[j];
+	}
+	row->render[idx] = '\0';
+	row->rsize = idx;
+}
+
 void editorAppendRow(char *s, size_t len){
 	E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
 
@@ -174,6 +187,7 @@ void editorAppendRow(char *s, size_t len){
 
 	E.row[at].rsize = 0;
 	E.row[at].render = NULL;
+	editorUpdateRow(&E.row[at]);
 
 	E.numrows++;
 }
