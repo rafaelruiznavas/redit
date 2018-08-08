df9f86d398c3d6d77346a874f74a0dcc818c2783
Rafael Ruiz Navas <rafa@ruiznavas.com>
2018-08-08 10:33:38 +0200

    Agrego coloff, para manejar el scroll horizontal. Actualizo la funcion editorDrawRows para que use el offset de scroll horizontal Actualizo la funcion editorScroll para que actualice coloff

--- redit.c
+++ redit.c
@@ -37,6 +37,7 @@ typedef struct erow{
 struct editorConfig{
 	int cx, cy;
 	int rowoff;
+	int coloff;
 	int screenrows;
 	int screencols;
 	int numrows;
@@ -218,6 +219,12 @@ void editorScroll(){
 	if(E.cy >= E.rowoff + E.screenrows){
 		E.rowoff = E.cy - E.screenrows + 1;
 	}
+	if(E.cx < E.coloff){
+		E.coloff = E.cx;
+	}
+	if(E.cx >= E.coloff + E.screencols){
+		E.coloff = E.cx - E.screencols + 1;
+	}
 }
 
 void editorDrawRows(struct abuf *ab){
@@ -240,9 +247,10 @@ void editorDrawRows(struct abuf *ab){
 				abAppend(ab,"~",1);
 			}
 		} else {
-			int len = E.row[filerow].size;
+			int len = E.row[filerow].size - E.coloff;
+			if(len < 0) len = 0;
 			if(len > E.screencols) len = E.screencols;
-			abAppend(ab, E.row[filerow].chars, len);
+			abAppend(ab, &E.row[filerow].chars[E.coloff], len);
 		}
 
 		abAppend(ab,"\x1b[K",3);
@@ -329,6 +337,7 @@ void initEditor(){
 	E.cx = 0;
 	E.cy = 0;
 	E.rowoff = 0;
+	E.coloff = 0;
 	E.numrows = 0;
 	E.row = NULL;
 	if(getWindowSize(&E.screenrows, &E.screencols) == -1)
