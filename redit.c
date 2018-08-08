b25290401a74b12347174068094937b788535815
Rafael Ruiz Navas <rafa@ruiznavas.com>
2018-08-09 00:26:20 +0200

    Corregido comportamiento del cursor en los tabuladores

--- redit.c
+++ redit.c
@@ -40,6 +40,7 @@ typedef struct erow{
 
 struct editorConfig{
 	int cx, cy;
+	int rx;
 	int rowoff;
 	int coloff;
 	int screenrows;
@@ -165,6 +166,17 @@ int getWindowSize(int *rows, int *cols){
 }
 
 /*** Operaciones de filas ***/
+int editorRowCxToRx(erow *row, int cx){
+	int rx = 0;
+	int j;
+	for(j=0;j<cx;j++){
+		if(row->chars[j] == '\t')
+			rx += (REDIT_TAB_STOP - 1) - (rx % REDIT_TAB_STOP);
+		rx++;		
+	}
+	return rx;
+}
+
 void editorUpdateRow(erow *row){
 	int tabs = 0;
 	int j;
@@ -244,17 +256,21 @@ void abFree(struct abuf *ab){
 
 /*** Salida ***/
 void editorScroll(){
+	E.rx = 0;
+	if(E.cy < E.numrows)
+		E.rx = editorRowCxToRx(&E.row[E.cy], E.cx);
+
 	if(E.cy < E.rowoff){
 		E.rowoff = E.cy;
 	}
 	if(E.cy >= E.rowoff + E.screenrows){
 		E.rowoff = E.cy - E.screenrows + 1;
 	}
-	if(E.cx < E.coloff){
-		E.coloff = E.cx;
+	if(E.rx < E.coloff){
+		E.coloff = E.rx;
 	}
-	if(E.cx >= E.coloff + E.screencols){
-		E.coloff = E.cx - E.screencols + 1;
+	if(E.rx >= E.coloff + E.screencols){
+		E.coloff = E.rx - E.screencols + 1;
 	}
 }
 
@@ -301,7 +317,7 @@ void editorRefreshScreen(){
 	editorDrawRows(&ab);
 
 	char buf[32];
-	snprintf(buf,sizeof(buf),"\x1b[%d;%dH",(E.cy - E.rowoff)+1,(E.cx - E.coloff)+1);
+	snprintf(buf,sizeof(buf),"\x1b[%d;%dH",(E.cy - E.rowoff)+1,(E.rx - E.coloff)+1);
 	abAppend(&ab,buf,strlen(buf));
 
 	abAppend(&ab,"\x1b[?25h",6);
@@ -381,6 +397,7 @@ void editorProcessKeypress(){
 void initEditor(){
 	E.cx = 0;
 	E.cy = 0;
+	E.rx = 0;
 	E.rowoff = 0;
 	E.coloff = 0;
 	E.numrows = 0;
