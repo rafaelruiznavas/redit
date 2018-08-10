2339d9e8f538ccbef6cf9427c03249c428c7e6b8
Rafael Ruiz Navas <rafa@ruiznavas.com>
2018-08-10 16:56:33 +0200

    Incrementamos el flag dirty cada vez que se realiza una operación que modifique el fichero abierto

--- redit.c
+++ redit.c
@@ -225,6 +225,7 @@ void editorAppendRow(char *s, size_t len){
 	editorUpdateRow(&E.row[at]);
 
 	E.numrows++;
+	E.dirty++;
 }
 
 void editorRowInsertChar(erow *row, int at, int c){
@@ -234,6 +235,7 @@ void editorRowInsertChar(erow *row, int at, int c){
 	row->size++;
 	row->chars[at] = c;
 	editorUpdateRow(row);
+	E.dirty++;
 }
 
 /*** Operaciones del editor ***/
