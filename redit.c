213897480dd572bc98dc3502145f8cf81df60919
Rafael Ruiz Navas <rafa@ruiznavas.com>
2018-08-10 10:43:59 +0200

    Agregada funcion editorRowsToString, que convierte los row del editor en un cadena de texto

--- redit.c
+++ redit.c
@@ -240,6 +240,24 @@ void editorInsertChar(int c){
 }
 
 /*** Entrada/Salida Ficheros ***/
+char *editorRowsToString(int *buflen){
+	int totlen = 0;
+	int j;
+	for(j=0;j<E.numrows;j++)
+		totlen += E.row[j].size+1;
+	*buflen = totlen;
+
+	char *buf = malloc(totlen);
+	char *p = buf;
+	for(j=0;j<E.numrows;j++){
+		memcpy(p, E.row[j].chars, E.row[j].size);
+		p += E.row[j].size;
+		*p = '\n';
+		p++;
+	}
+	return buf;
+}
+
 void editorOpen(char *filename){
 	free(E.filename);
 	E.filename = strdup(filename);
