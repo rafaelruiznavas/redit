8f49a7b3567eb77d1a66249aaf7c20ba56989f31
Rafael Ruiz Navas <rafa@ruiznavas.com>
2018-08-10 18:20:20 +0200

    Agregamos la funcion editorRowDelChar para borrar un caracter en una posicion dada

--- redit.c
+++ redit.c
@@ -239,6 +239,14 @@ void editorRowInsertChar(erow *row, int at, int c){
 	E.dirty++;
 }
 
+void editorRowDelChar(erow *row, int at){
+	if(at < 0 || at > row->size) return;
+	memmove(&row->chars[at],&row->chars[at+1],row->size - at);
+	row->size--;
+	editorUpdateRow(row);
+	E.dirty++;
+}
+
 /*** Operaciones del editor ***/
 void editorInsertChar(int c){
 	if(E.cy == E.numrows){
